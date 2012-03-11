732d4c7e5ffdbaf1ee3cd930906be27ac4983561
kristinh <kristinh@precis-project.com>
2012-03-11 23:12:03 +0100

    Added session so that filter and sort persist

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,12 @@ class MoviesController < ApplicationController
   end
 
   def index
+
+    if params[:sort].nil? && params[:ratings].nil? &&
+        (!session[:sort].nil? || !session[:ratings].nil?)
+      redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
+    end
+
     @sort = params[:sort]
     @ratings = params[:ratings] 
     if @ratings.nil?
@@ -30,6 +36,9 @@ class MoviesController < ApplicationController
     else
       @movies = Movie.find_all_by_rating(ratings)
     end
+
+    session[:sort] = @sort
+    session[:ratings] = @ratings
   end
 
   def new
